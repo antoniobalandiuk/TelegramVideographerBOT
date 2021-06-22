0954ab0f5e4efc92f755f29d7ab8e085de9518b9
Antonio <merlintube7@gmail.com>
2021-06-22 03:18:05 +0300

    example

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.exceptions import MessageNotModified
 from config import TOKEN
 
 bot = Bot(token=TOKEN)
@@ -14,7 +15,7 @@ services = InlineKeyboardButton(text='Види послуг 🎥', callback_data
 price = InlineKeyboardButton(text='Прайс-лист 💵', callback_data="price")
 contacts = InlineKeyboardButton(text='Контактні дані 👥', callback_data="contacts")
 special_offers = InlineKeyboardButton(text='Особливі пропозиції 🤝', callback_data="special_offers")
-reviews = InlineKeyboardButton(text='Відгуки', callback_data="reviews")
+reviews = InlineKeyboardButton(text='Відгуки 🗣', callback_data="reviews")
 keyboard.add(services, price, contacts, special_offers, reviews)
 
 
@@ -24,47 +25,6 @@ async def process_start_command(message: types.Message):
                         f'Я – Анатолій Ружицький, мені 16 років👦. \"16?! Та ти обманюєш!\" - часто чую такі реакції з боку своїх клієнтів, які вподальшому говорять: \"Вірний своїй справі, вірний своїм переконанням, бомбезний оператор, в якого все ще попереду!\n\n'
                         f'Моя робота — створити приємні спогади, відтворити атмосферу подій на природі🛤, у місті🌇 чи студії🎼, запам`ятати усі емоції в кадрах.',
                         reply_markup=keyboard)
-
-
-# const callbacks = {
-#     menu: () => {},
-#     price: () => {},
-#     default: () =>
-# };
-#
-#  callbacks[call](ьуыыфпу) || [default]
-
-
-# Callbecks for telegram call : string
-
-# @dp.callback_query_handler(text_contains='menu_')
-# async def menu(call: types.CallbackQuery):
-#     if call.data and call.data.startswith("menu_"):
-#         code = call.data[-1:]
-#         if code.isdigit():
-#             code = int(code)
-#         if code == 1:
-#             await call.message.edit_text(f'Тут будуть послуги, моделі камер та дрону', reply_markup=keyboard)
-#         if code == 2:
-#             await call.message.edit_text(f'🎞  Кліпи - від 200💲\n\n'
-#                                          f'👗  Fashion - від 100💲\n\n'
-#                                          f'🗣  Рекламні відео - від 100💲\n\n'
-#                                          f'💍  Весільні фільми - є кілька пакетів, залежно від кількості знімальних годин, камер та наявності аерозйомки\n\n'
-#                                          f'💏  Love Story - 100💲\n\n'
-#                                          f'✈  Аерозйомка - від 50💲\n\n'
-#                                          f'👨‍💻  Монтаж - від 30💲\n\n', reply_markup=keyboard)
-#         if code == 3:
-#             await call.message.edit_text(f'📱Моб.тел - +380975547372\n\n'
-#                                          f'Instagram - @tolian_ruzhytskyi\n\n'
-#                                          f'Telegram - @tolian_ruzhytskyi\n\n', reply_markup=keyboard)
-#         if code == 4:
-#             await call.message.edit_text(f'Я є прибічником збалансованого обміну\n'
-#                                          f'послугами або ж товарами, тому якщо\n'
-#                                          f'у вас є вигідна бартерна пропозиція,\n'
-#                                          f'то дзвоніть по номеру телефону, що\n'
-#                                          f'вказаний у меню "Контактні дані 👥"\n', reply_markup=keyboard)
-#         else:
-#             await bot.answer_callback_query(call.id)
 
 
 @dp.callback_query_handler(text_contains='services')
@@ -104,22 +64,45 @@ async def menu(call: types.CallbackQuery):
     await call.message.edit_text('Перейдіть по силці і залишіть коментар', reply_markup=keyboard)
 
 
+keyboard_for_example = InlineKeyboardMarkup()
+klip = InlineKeyboardButton(text='Кліп 🎞', callback_data="klip")
+fashion = InlineKeyboardButton(text='Fashion 👗', callback_data="fashion")
+promotional_videos = InlineKeyboardButton(text='Рекламні відео 🗣', callback_data="promotional_videos")
+wedding_videos = InlineKeyboardButton(text='Весільні відео 💍', callback_data="wedding_videos")
+love_story = InlineKeyboardButton(text='Love story 💏', callback_data="love_story")
+aerovideo = InlineKeyboardButton(text='Аерозйомка ✈', callback_data="aerovideo")
+video_editing = InlineKeyboardButton(text='Монтаж 👨‍💻', callback_data="video_edition")
+keyboard_for_example.add(klip, fashion, promotional_videos, wedding_videos, love_story, aerovideo, video_editing)
+
+
 @dp.message_handler(commands=['example'])
-async def process_example_command(message: types.Message):
-    await message.reply("Виберіть тип відео, приклад якого ви хочите побачити")
+async def message_before_example(message: types.Message):
+    await message.reply('Ви увыйшли в меню "/example"'
+                        'Натискаючи на кнопки під цим текстом ви вибираєте'
+                        'тип відео, яке ви хочете переглянути', reply_markup=keyboard_for_example)
 
-    await asyncio.sleep(1)
 
-    await types.ChatActions.upload_video()
+@dp.errors_handler(exception=MessageNotModified)
+async def message_not_modified_handler(update, error):
+    return True
 
-    media = types.MediaGroup()
-
-    media.attach_video(types.inputFile('example/video/222.mp4'))
-
-    await message.reply_media_group(media=media)
+"""Це надсилає відео в бота"""
+# @dp.message_handler(text_contains='example_video')
+# async def process_example_command(call: types.CallbackQuery):
+#     await call.message.edit_text('Виберіть тип відео, приклад якого ви хочите побачити', reply_markup=keyboard)
+#
+#     await asyncio.sleep(1)
+#
+#     await types.ChatActions.upload_video()
+#
+#     media = types.MediaGroup()
+#
+#     media.attach_video(types.InputFile('example/video/advert.mp4'))
+#
+#     await call.message.reply_media_group(media=media)
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
 
 
